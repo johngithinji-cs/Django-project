2f126b515cfbb2e164856c534dd90cc35b58214a
johngithinji-cs <mathaijohn97@gmail.com>
2024-10-28 12:13:34 +0300

    template commit

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
